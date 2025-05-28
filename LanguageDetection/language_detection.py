6b5aaee6a492603e61b27a36909ae557917487df
Jokku-gamma <joelkuaba2004@gmail.com>
2025-05-28 22:08:55 +0530

    Updated code

--- LanguageDetection/language_detection.py
+++ LanguageDetection/language_detection.py
@@ -4,13 +4,16 @@ import sounddevice as sd
 import scipy.io.wavfile as wavfile
 from datetime import datetime
 from flask import Flask,request,jsonify
+from transformers import pipeline
 import os
+from dotenv import load_dotenv
+load_dotenv()
 app=Flask(__name__)
 model=whisper.load_model("base")
 SAMPLE_RATE=16000
 DURATION=10
 OUTPUT_FILE="recorded_audio.wav"
-
+translator = pipeline("translation", model="Helsinki-NLP/opus-mt-mul-en",token=os.environ.get("HUGGING_FACE_AUTH_TOKEN"))
 def record():
     print(f"Recording for {DURATION} seconds")
     audio=sd.rec(int(DURATION * SAMPLE_RATE),samplerate=SAMPLE_RATE,channels=1)
@@ -32,18 +35,21 @@ def detect(audio_path):
     res=model.transcribe(audio_path,language=detected_lang)
     trans=res["text"]
 
-    return detected_lang,conf,trans
+    translation = trans if detected_lang == "en" else translator(trans)[0]["translation_text"]
+    return detected_lang,conf,trans,translation
 
 @app.route('/detect-language',methods=['POST'])
 def detect_endpoint():
     try:
-        if 'audio' in request.files:
+        if 'audio' not in request.files:
+            return jsonify({"status":"error","message":"No audio file provided"}),400
+        else:
             audio_file=request.files['audio']
             audio_path=os.path.join("uploads",audio_file.filename)
             os.makedirs("uploads",exist_ok=True)
             audio_file.save(audio_path)
-        else:
-            audio_path=record()
+        '''else:
+            audio_path=record()'''
         det_lang,conf,_,_=detect(audio_path)
         if os.path.exists(audio_path) and audio_path!=OUTPUT_FILE:
             os.remove(audio_path)
@@ -59,13 +65,15 @@ def detect_endpoint():
 @app.route('/transcription',methods=['POST'])
 def transcription_endpoint():
     try:
-        if 'audio' in request.files:
+        if 'audio' not in request.files:
+            return jsonify({"status":"error","message":"No audio file provided"}),400
+        else:
             audio_file=request.files['audio']
             audio_path=os.path.join("uploads",audio_file.filename)
             os.makedirs("uploads",exist_ok=True)
             audio_file.save(audio_path)
-        else:
-            audio_path=record()
+        '''else:
+            audio_path=record()'''
         _,_,trans,_=detect(audio_path)
         if os.path.exists(audio_path) and audio_path!=OUTPUT_FILE:
             os.remove(audio_path)
@@ -80,13 +88,15 @@ def transcription_endpoint():
 @app.route('/translation',methods=['POST'])
 def translation_endpoint():
     try:
-        if 'audio' in request.files:
+        if 'audio' not in request.files:
+            return jsonify({"status":"error","message":"No audio file provided"}),400
+        else:
             audio_file=request.files['audio']
             audio_path=os.path.join("uploads",audio_file.filename)
             os.makedirs("uploads",exist_ok=True)
             audio_file.save(audio_path)
-        else:
-            audio_path=record()
+        '''else:
+            audio_path=record()'''
         det_lang,_,_,trans=detect(audio_path)
         if os.path.exists(audio_path) and audio_path!=OUTPUT_FILE:
             os.remove(audio_path)
@@ -100,3 +110,4 @@ def translation_endpoint():
 
 if __name__=="__main__":
     app.run(debug=True,host="0.0.0.0",port=5000)
+    
